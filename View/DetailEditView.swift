8ba11e4abd8fc3ebacf53ceae7d84de38ac823c4
rowjoy <rowjoy202@gmail.com>
2025-10-09 19:15:39 +0600

    Day 2

---
+++ View/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Jamirul Islam on 9/10/25.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
---
+++ View/DetailVIew.swift
@@ -0,0 +1,18 @@
+//
+//  DetailVIew.swift
+//  Scrumdinger
+//
+//  Created by Jamirul Islam on 8/10/25.
+//
+
+import SwiftUI
+
+struct DetailVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailVIew()
+}
