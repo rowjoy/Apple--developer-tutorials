f221fb42537cddc671f6d898d1097296ecf68fed
rowjoy <rowjoy202@gmail.com>
2025-10-14 20:16:37 +0600

    Passing data with binding

---
+++ View/ThemePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Jamirul Islam on 14/10/25.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemePicker()
+}
---
+++ View/ThemeView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Jamirul Islam on 14/10/25.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeView()
+}
